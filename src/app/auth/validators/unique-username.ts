23911ff7cde0e4191d277a8160c982493a7485f3
Miha Mulej <miha.mulej@petrol.si>
2021-11-17 13:40:05 +0100

    update on the email client app

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AbstractControl, AsyncValidator } from '@angular/forms';
 import { map } from 'rxjs';
+
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   validate = (control: AbstractControl) => {
     const { value } = control;
-    console.log(this.http);
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value,
+    return this.authService.usernameAvailable(value).pipe(
+      map(() => {
+        console.log(value);
+        return null;
       })
-      .pipe(
-        map(() => {
-          console.log(value);
-          return null;
-        })
-      );
+    );
   };
 }
